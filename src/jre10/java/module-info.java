f38881017de1f672a17c89082adb61fc46afd5d6
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:43:53 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,7 @@
+module com.jwebmp.plugins.bootstrap.dialog {
+	exports com.jwebmp.plugins.bootstrap.dialog;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+}
