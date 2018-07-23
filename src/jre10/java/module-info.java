ceb9b9575dc81b2e98d3e2058d9c3dbd89a26fd1
GedMarc <ged_marc@hotmail.com>
2018-07-23 19:34:21 +0200

    JDK 10 Service Loading rather than Classpath Search (Much Faster), Build update to classify JDK 10 dependencies properly

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,7 +1,12 @@
+import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.plugins.bootstrap.dialog.BSDialogPageConfigurator;
+
 module com.jwebmp.plugins.bootstrap.dialog {
 	exports com.jwebmp.plugins.bootstrap.dialog;
 
 	requires com.jwebmp.core;
 	requires com.jwebmp.logmaster;
 	requires com.fasterxml.jackson.annotation;
+
+	provides IPageConfigurator with BSDialogPageConfigurator;
 }
