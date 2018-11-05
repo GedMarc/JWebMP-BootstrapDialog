91069d993c82ef7d9c2243f8be4a1085f5a7ad44
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:43 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,4 +1,6 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.bootstrap.dialog.BSDialogPageConfigurator;
 
 module com.jwebmp.plugins.bootstrap.dialog {
@@ -8,8 +10,12 @@ module com.jwebmp.plugins.bootstrap.dialog {
 	requires com.jwebmp.logmaster;
 	requires com.fasterxml.jackson.annotation;
 	requires java.validation;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with BSDialogPageConfigurator;
+
+	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.bootstrap.dialog.implementations.BootstrapDialogExclusionsModule;
+	provides IGuiceScanJarExclusions with com.jwebmp.plugins.bootstrap.dialog.implementations.BootstrapDialogExclusionsModule;
 
 	opens com.jwebmp.plugins.bootstrap.dialog to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
