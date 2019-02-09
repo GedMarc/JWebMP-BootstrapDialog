8e31b827f82ae199cc4632471ad4a77a233db53f
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:11 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/bootstrap4/dialog/BSDialogPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/bootstrap4/dialog/BSDialogPageConfigurator.java
@@ -83,7 +83,7 @@ public class BSDialogPageConfigurator
 	@SuppressWarnings("unchecked")
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			page.getBody()
 			    .getJavascriptReferences()
